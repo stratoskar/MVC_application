49df049575b5c3cffb73ee4b26d4e0d471702036
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-01-07 14:28:55 +0200

    added controllers and layouts for each user

--- WhereIsMyGrade/Controllers/HomeController.cs
+++ WhereIsMyGrade/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace WhereIsMyGrade.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         public IActionResult Login()
         {
             return View();
---
+++ WhereIsMyGrade/Controllers/SecretaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhereIsMyGrade.Controllers
+{
+    public class SecretaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

d72e8888944559ee5c501363197e8ad6c0e46003
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2023-01-10 16:01:28 +0200

    Just playing

--- WhereIsMyGrade/Controllers/HomeController.cs
+++ WhereIsMyGrade/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
+using WhereIsMyGrade.Data;
 using WhereIsMyGrade.Models;
 
 namespace WhereIsMyGrade.Controllers
@@ -7,14 +9,27 @@ namespace WhereIsMyGrade.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+            // Take the username and password that user inserted
+            string GivenUsername = Request.Form["username"].ToString();
+            string GivenPassword = Request.Form["password"].ToString();
+
+            var obj = _db.user.ToList();
+
+            if (GivenUsername.Equals("sk"))
+            {
+                return RedirectToAction("Index"); // Go to the main page if user's input is valid
+            }
+          
             return View();
         }
 

7e7e56d759d4fbf1f5510d9fef5554313fd02a68
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-01-11 00:29:51 +0200

    professors can now be added
    NEED TO FIX AFM PROBLEM

--- WhereIsMyGrade/Controllers/SecretaryController.cs
+++ WhereIsMyGrade/Controllers/SecretaryController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhereIsMyGrade.Data;
+using WhereIsMyGrade.Models;
 
 namespace WhereIsMyGrade.Controllers
 {
     public class SecretaryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public SecretaryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -20,7 +29,54 @@ namespace WhereIsMyGrade.Controllers
         /// <returns></returns>
         public IActionResult RegisterUserToDatabase()
         {
+            // get the user type.
             string user_type = Request.Form["user_selection"].ToString();
+
+            switch (user_type)
+            {
+                case "professor":
+
+                    // retrieve data from the form.
+                    string username = Request.Form["username"].ToString();
+                    string password = Request.Form["password"].ToString();
+                    string firstname = Request.Form["first_name"].ToString();
+                    string lastname = Request.Form["last_name"].ToString();
+                    string department = Request.Form["department"].ToString();
+                    string AFM = Request.Form["AFM"].ToString();
+
+                    // create a user object
+                    users user = new users();
+
+                    user.Username = username;
+                    user.Password = password;
+                    user.Role = "professor";
+                    _db.user.Add(user);
+
+                    // create a professor object
+                    professors professor = new professors();
+
+                    //professor.AFM = int.Parse(AFM);
+                    professor.USERS_username = username;
+                    professor.Name = firstname;
+                    professor.Surname = lastname;
+                    professor.Department = department;
+                    _db.professors.Add(professor);
+
+                    // add to the database
+                    _db.SaveChanges();
+                    break;
+
+                case "student":
+                    break;
+
+                case "course":
+                    break;
+
+                default:
+                    return RedirectToAction("Error");
+
+            }
+
             return View();
         }
     }

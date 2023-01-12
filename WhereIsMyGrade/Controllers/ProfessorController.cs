20086e58d6cbd82b86ab0e4acd0c22fa42b24179
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-01-12 16:20:56 +0200

    fully functional displaying grades system

--- WhereIsMyGrade/Controllers/ProfessorController.cs
+++ WhereIsMyGrade/Controllers/ProfessorController.cs
@@ -27,12 +27,16 @@ namespace WhereIsMyGrade.Controllers
 
         public IActionResult DisplayGrades()
         {
+            // in case the user decides to refresh the page, keep the AFM and professor name values
             TempData.Keep("Name");
             TempData.Keep("AFM");
 
+            // retrieve all necessary data for the table to be shown
             var professor_courses = (from course in _db.course.ToList() where course.PROFESSORS_AFM == int.Parse(TempData["AFM"].ToString()) select course).ToList();
             var student_grades = _db.course_has_students.ToList().FindAll(x => (from course in professor_courses select course.IdCourse).ToList().Contains(x.COURSE_idCOURSE));
+            student_grades = (from grade in student_grades where grade.GradeCourseStudent != -1 select grade).ToList();
 
+            // save the data
             dynamic model = new ExpandoObject();
 
             model.professor_courses = professor_courses;
